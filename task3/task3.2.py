bd17bfa4a59587c8dc531d94b5a64cffc96f9c33
artem <a.y.logvin@gmail.com>
2020-11-01 15:57:17 +0300

    Сделал задание 2

--- task3/task3.2.py
+++ task3/task3.2.py
@@ -3,3 +3,23 @@
  город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
  Реализовать вывод данных о пользователе одной строкой.
 """
+
+def user_info(name, surname, year, city, email, telephone):
+    year = str(year)
+    telephone = str(telephone)
+    result = ' '.join([surname, name, year, city, email, telephone])
+    print(result)
+    return ' '.join([surname, name, year, city, email, telephone])
+
+
+
+
+if __name__ == '__main__':
+    name = input('Введите имя')
+    surname = input('Введите фамилию')
+    year = int(input('Введите Ваш год рождения'))
+    city = input('Введите город проживания')
+    email = input('Введите Ваш email')
+    telephone = input('Введите Ваш номер телефона')
+
+    user_info(name, surname, year, city, email, telephone)
