0d67f1a4df9a0234b2c84bb52eb18e6474e4464f
artem <a.y.logvin@gmail.com>
2020-11-01 14:33:43 +0300

    Сделал первое задание

--- task3/task3.1.py
+++ task3/task3.1.py
@@ -3,3 +3,22 @@
 Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
 
+
+def division(numetor, denomitor):
+    if not numetor.isalpha() and not denomitor.isalpha():
+        num = int(numetor)
+        den = int(denomitor)
+        try:
+            result = num / den
+            print('результат: ', result)
+            return result
+        except ZeroDivisionError:
+            print('Вы пытались разделить на ноль! Не надо так')
+    else:
+        print('Вы ввели не чилсло(а)')
+
+if __name__ == '__main__':
+    num = input('Введите числитель')
+    den = input('Введите знаменатель')
+
+    division(num, den)
