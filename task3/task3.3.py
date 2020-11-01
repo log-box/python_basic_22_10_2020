c03020a66ccc1801000e522a489099b0a3cfbeb4
artem <a.y.logvin@gmail.com>
2020-11-01 16:36:10 +0300

    Сделал заание 3

--- task3/task3.3.py
+++ task3/task3.3.py
@@ -3,3 +3,21 @@
 которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
 """
 
+
+def my_func(arg1: int, arg2: int, arg3: int):
+    if arg1 >= arg3 and arg2 >= arg3:
+        print(arg1 + arg2)
+        return arg1 + arg2
+    elif arg1 > arg2 and arg2 < arg3:
+        print(arg1 + arg3)
+        return arg1 + arg3
+    else:
+        print(arg2 + arg3)
+        return arg2 + arg3
+
+
+if __name__ == '__main__':
+    arg1 = int(input('Введите число 1'))
+    arg2 = int(input('Введите число 2'))
+    arg3 = int(input('Введите число 3'))
+    my_func(arg1, arg2, arg3)
