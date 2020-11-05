31e5461c0348958da2c20599bcf4757c5a4718f7
artem <a.y.logvin@gmail.com>
2020-11-05 07:13:23 +0300

    Сделал задание 4

--- les4/task4.4.py
+++ les4/task4.4.py
@@ -7,3 +7,8 @@
 Результат: [23, 1, 3, 10, 4, 11]
 """
 
+my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+my_new_list = [elm for elm in my_list if my_list.count(elm) < 2]
+
+print(my_new_list)
