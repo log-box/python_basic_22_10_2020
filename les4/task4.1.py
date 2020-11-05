f38f4cf17a132099d8d18cfdd9fbd3ae17125626
artem <a.y.logvin@gmail.com>
2020-11-05 06:46:19 +0300

    Сделал первое задание

--- les4/task4.1.py
+++ les4/task4.1.py
@@ -3,3 +3,23 @@
 В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 """
+
+
+from sys import argv
+
+def salary():
+    try:
+
+        time_works = float(argv[1])
+        salary_per_hour = float(argv[2])
+        bonus = float(argv[3])
+
+        salary = round(((time_works * salary_per_hour) + bonus), 2)
+        print(f'Зарплата сотрудника равна: {salary}')
+
+    except ValueError:
+        print('Вы ввели не число(а)')
+
+
+if __name__ == '__main__':
+    salary()
