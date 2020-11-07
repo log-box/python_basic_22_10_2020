feebb9d27f1e804009906902fc128f7878769dde
artem <a.y.logvin@gmail.com>
2020-11-07 10:24:26 +0300

    Сделала первое задание

--- les5/task5.1.py
+++ les5/task5.1.py
@@ -3,3 +3,12 @@
 Об окончании ввода данных свидетельствует пустая строка.
 """
 
+my_file = open('task5.1_file.txt', 'w')
+line = input('Введите текст\n') + '\n'
+while line:
+    my_file.writelines(line)
+    line = input('Введите текст\n') + '\n'
+    if line == '\n':
+        my_file.close()
+        break
+
