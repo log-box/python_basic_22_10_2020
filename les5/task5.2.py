c055337def916f71211d1880d8bf3373314882f1
artem <a.y.logvin@gmail.com>
2020-11-07 11:47:25 +0300

    Сделал задание 2

--- les5/task5.2.py
+++ les5/task5.2.py
@@ -3,3 +3,14 @@
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
 
+my_file = open('task5.2_file.txt', 'r')
+content = my_file.readlines()
+my_file.close()
+print(f'Количество строк в файле {len(content)}')
+my_file = open('task5.2_file.txt', 'r')
+
+x = 1
+for line in my_file:
+    print(f'Количеыство слов в строке №{x} равно: {len(line.split())}')
+    x += 1
+my_file.close()
