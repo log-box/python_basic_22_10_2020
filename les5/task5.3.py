a4885445d4bafcab02c342ab58c42f97fa04a260
artem <a.y.logvin@gmail.com>
2020-11-08 09:21:06 +0300

    сделал 3 задание

--- les5/task5.3.py
+++ les5/task5.3.py
@@ -3,3 +3,22 @@
 Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 Выполнить подсчет средней величины дохода сотрудников.
 """
+
+my_file = open('task5.3_file.txt', 'r')
+
+names = []
+average = 0
+i = 0
+for line in my_file:
+    worker = line.split()
+    if int(worker[1]) < 20000:
+        names.append(worker[0])
+    average = average + int(worker[1])
+    i += 1
+
+print('===Бедолаги===')
+for name in names:
+
+    print(name)
+print('==============')
+print(f'Средняя зарплата равна: {round(average/i,2)}')
