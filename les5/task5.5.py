f17ec1ca5ee2734881ed42e75d86b3f597d0518b
artem <a.y.logvin@gmail.com>
2020-11-08 12:07:18 +0300

    сделал задание 5

--- les5/task5.5.py
+++ les5/task5.5.py
@@ -2,6 +2,12 @@
 Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
-
-
+with open('task5.5_file.txt', 'w') as file:
+    try:
+        line = input('Введите числа разделенные пробелами\n')
+        file.writelines(line)
+        numbers = line.split()
+        print(sum(map(int,numbers)))
+    except ValueError:
+        print('Вы ввели не число(а)')
 
