8cb91ef6007799ef95b44714c45312029f06cca7
artem <a.y.logvin@gmail.com>
2020-11-12 19:20:07 +0300

    Сделал задание 6 и все задания!

--- task6.5.py
+++ task6.5.py
@@ -6,3 +6,41 @@
 Для каждого из классов методы должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+class Stationery:
+    def __init__(self, title='Пальцем'):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+class Pen(Stationery):
+    def __init__(self, title='Пальцем'):
+        super().__init__(title)
+        self.title = Pen.__name__
+
+    def draw(self):
+        print(f'Запуск отрисовки c помощью {self.title}')
+
+class Pencil(Stationery):
+    def __init__(self, title='Пальцем'):
+        super().__init__(title)
+        self.title = Pencil.__name__
+    def draw(self):
+        print(f'Запуск отрисовки c помощью {self.title}')
+
+class Handle(Stationery):
+    def __init__(self, title='Пальцем'):
+        super().__init__(title)
+        self.title = Handle.__name__
+    def draw(self):
+        print(f'Запуск отрисовки c помощью {self.title}')
+
+x=Stationery()
+x.draw()
+a=Pen()
+a.draw()
+b=Pencil()
+b.draw()
+c=Handle()
+c.draw()
