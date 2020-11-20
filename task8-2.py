460e7db723dd761bc4b07a3f0d2f5ac3c57561bd
artem <a.y.logvin@gmail.com>
2020-11-20 09:16:33 +0300

    Сделал задание 2

--- task8-2.py
+++ task8-2.py
@@ -3,3 +3,24 @@
 вводимых пользователем. При вводе пользователем нуля в качестве делителя программа должна корректно обработать эту
 ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyError(Exception):
+    def __init__(self, _str):
+        self.str = _str
+
+def division(arg1, arg2):
+    try:
+        if arg2 == 0:
+            raise MyError('Вы пытаетесь делить на ноль')
+        else:
+            res = arg1 / arg2
+            return res
+
+    except MyError as err:
+        return err
+
+a = division(1,2)
+print(a)
+b = division(1,0)
+print(b)
